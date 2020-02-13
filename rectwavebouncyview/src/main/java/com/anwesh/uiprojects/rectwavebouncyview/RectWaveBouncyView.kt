0501a9d8fc2f55c25ba31fa7755b06c1c0e899cc
Anwesh43 <anweshthecool0@gmail.com>
2020-02-13 10:31:01 +0530

    adding constants and libraries

--- rectwavebouncyview/src/main/java/com/anwesh/uiprojects/rectwavebouncyview/RectWaveBouncyView.kt
+++ rectwavebouncyview/src/main/java/com/anwesh/uiprojects/rectwavebouncyview/RectWaveBouncyView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rectwavebouncyview
 /**
  * Created by anweshmishra on 13/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val scGap : Float = 0.02f
