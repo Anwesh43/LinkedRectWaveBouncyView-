6f7ca1f32887268b770f4f27f310c44843e93e29
Anwesh43 <anweshthecool0@gmail.com>
2020-02-13 11:01:28 +0530

    adding renderer in view

--- rectwavebouncyview/src/main/java/com/anwesh/uiprojects/rectwavebouncyview/RectWaveBouncyView.kt
+++ rectwavebouncyview/src/main/java/com/anwesh/uiprojects/rectwavebouncyview/RectWaveBouncyView.kt
@@ -61,14 +61,16 @@ fun Canvas.drawRWNode(i : Int, scale : Float, paint : Paint) {
 
 class RectWaveBouncyView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -191,8 +193,9 @@ class RectWaveBouncyView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val brw : BouncyRectWave = BouncyRectWave(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             brw.draw(canvas, paint)
             animator.animate {
